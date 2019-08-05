c64c2b55326d88056406fe9f603c5b25a1acfce1
limsungju <fullmoon4860@gmail.com>
2019-08-05 17:51:22 +0900

    init

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,5 +3,9 @@ package helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
